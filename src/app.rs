6afe81ca4ef8213157afea729316bddddfc19550
Arne Berner <arne.berner@student.fh-kiel.de>
2025-09-24 01:52:58 +0200

    fix: cupcake looks better than lemonade

--- src/app.rs
+++ src/app.rs
@@ -12,7 +12,7 @@ use crate::subpage::SubPage;
 pub fn shell(options: LeptosOptions) -> impl IntoView {
     view! {
       <!DOCTYPE html>
-      <html lang="de" data-theme="lemonade">
+      <html lang="de" data-theme="cupcake">
         <head>
           <meta charset="utf-8" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
